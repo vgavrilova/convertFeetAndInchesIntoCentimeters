515eee0f64bc3b89291c7604afff67cbbaed5d69
vgavrilova <gavrivalery@gmail.com>
2020-05-24 18:25:00 +0200

    Bug Fix

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,12 @@ public class Main {
         // of the calcFeetAndInchesToCentimeters() Method
         // If the method returned -1 (an invalid inserted value)
         // the program prints "Invalid Value. Try again!" out
+        // The inserted number of inches must be in a range between 0 and 12
 
-        double check = calcFeetAndInchesToCentimeters(255.98, 12);
-        if (check >= 0) {
-            System.out.println("Your result: " + check + " CM");
-        } else if (check == -1){
+        double check = calcFeetAndInchesToCentimeters(5, 16);
+        if (check >= 0.0) {
+            System.out.println("The result of conversion into centimeters: " + check + " CM");
+        } else if (check == -1) {
             System.out.println("Invalid Value. Try again!");
         }
     }
@@ -18,9 +19,10 @@ public class Main {
     // in the Main Method Feet and Inches into Centimeters
 
     public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
-
-        return ((feet >= 0) && (inches >= 0)) ? (((feet * 12) +
+        System.out.println("You have inserted " + feet + " Feet and " + inches + " Inches");
+        return ((feet >= 0) && (inches >= 0)) && (inches <= 12) ? (((feet * 12) +
                 (calcFeetAndInchesToCentimeters(inches) * 12)) * 2.54d) : -1;
+
     }
 
     // An overloading Method for calculating feet
